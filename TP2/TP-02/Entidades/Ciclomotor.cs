84d30e8f732136d4e71fbfc720965db98a7dfd78
ferport <portillofernandobek@gmail.com>
2021-10-03 23:45:43 -0300

    Segundo TP

---
+++ TP2/TP-02/Entidades/Ciclomotor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color)
+            :base(chasis,marca,color)
+        {
+        }
+        
+        /// <summary>
+        /// Ciclomotor son 'Chico'
+        /// </summary>
+        protected ETamanio Tamanio
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CICLOMOTOR");
+            sb.AppendLine((string)this);
+            sb.AppendFormat("TAMAÑO : {0}", this.Tamanio);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
